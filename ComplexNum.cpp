cf1092f3dca964b21a7becea2de468dcb9fcd54c
Alisa010 <115400022+Alisa010@users.noreply.github.com>
2022-10-09 14:21:08 +0400

    Add files via upload

---
+++ ComplexNum.cpp
@@ -0,0 +1,46 @@
+#include "ComplexNum.h"
+#include <cmath>
+#include <iostream>
+
+ComplexNum::ComplexNum() {};
+
+ComplexNum::ComplexNum(const double& re)
+	:mRe{re}
+{}
+
+ComplexNum::ComplexNum(const double& re, const double& im)
+	: mRe{ re }
+	, mIm{ im }
+{}
+
+double ComplexNum::getModulus(){
+	return sqrt(mRe * mRe + mIm * mIm);
+}
+
+double ComplexNum::getRe() {
+	return mRe;
+}
+
+double ComplexNum::getIm() {
+	return mIm;
+}
+
+	// ComplexNum + ComplexNum, 
+  ComplexNum operator+(const ComplexNum& other){
+     return ComplexNum(other.mRe + mRe, other.mIm + mIm);
+}
+	// ComplexNum - ComplexNum, 
+  ComplexNum operator-(const ComplexNum& other){
+     return ComplexNum(other.mRe - mRe, other.mIm - mIm);
+}
+  // ComplexNum * k, where k is a constant
+  ComplexNum operator*(int k){
+     return ComplexNum(k * mRe, k * mIm);
+}
+	// and operator<<
+std::ostream &operator<<(std::ostream &out, ComplexNum &complex)
+{
+  out <<  complex.mRe <<  + "i" << complex.mIm << "\n";
+ 
+   return out;
+}
---
+++ ComplexNum.h
@@ -0,0 +1,35 @@
+#ifndef COMPLEX_NUM_H
+#define COMPLEX_NUM_H
+#include <iostream>
+
+class ComplexNum {
+private:
+	double mRe = 0; // real parth
+	double mIm = 0; // imaginary path 
+public:
+	ComplexNum();
+	ComplexNum(const double& a); 
+	ComplexNum(const double& a, const double& b);
+
+	double getModulus();
+ 
+	double getRe();
+
+	double getIm();
+
+	
+	// ComplexNum + ComplexNum, 
+  ComplexNum operator+(const ComplexNum& other);
+	// ComplexNum - ComplexNum, 
+  ComplexNum operator-(const ComplexNum& other);
+  // ComplexNum * k, where k is a constant
+  ComplexNum operator*(int k);
+	// and operator<<
+  friend std::ostream &operator<<(std::ostream& , ComplexNum&);
+
+
+	// 
+	
+};
+
+#endif // COMPLEX_NUM_H
