65936c1765c681ed45034b07a5ab3f7a0e07971f
Alisa010 <115400022+Alisa010@users.noreply.github.com>
2022-10-09 14:24:55 +0400

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,27 +5,26 @@
 
 int main(){
 
-	ComplexNum c1{4 , 3};
-	ComplexNum c2{5 , 6};
+  ComplexNum c1{4 , 3};
+  ComplexNum c2{5 , 6};
   ComplexNum c3{3 , 9};
   ComplexNum c4{8 , 9};
 
-	std::vector<ComplexNum> vec;
+  std::vector<ComplexNum> vec;
   vec.push_back(c1);
-	vec.push_back(c2);
-	vec.push_back(c3);
+  vec.push_back(c2);
+  vec.push_back(c3);
   vec.push_back(c4);
 
 	for(int i = 0; i < vec.size(); ++i){
 		for(int j = 0; j < vec.size() - 1 - i; ++j){
 			  if(vec[j] > vec[j + 1]){
-      			std::swap(vec[j] , vec[j + 1]);
-			}
-    }
-
-	}
+      			       std::swap(vec[j] , vec[j + 1]);
+			   }
+                 }
+         }
   for(int i = 0; i < vec.size() ; ++i){
           std::cout << vec[i] ;
-	}
+     }
 
 }
